8220795bfa9a18b2afead9a3da6ce9cfec5a3491
Hack Reactor Student <student@hackreactor.com>
2017-07-03 15:39:09 -0700

    firstcommit

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj) { 
+  var result = '';
+  if(typeof(obj)=== 'number' || typeof(obj) === 'boolean' || (typeof(obj)==='object' && obj === null)){
+    result += ''+obj;
+  } else if (typeof(obj)==='string'){
+    result += '"'+obj+'"';
+  } else if (Array.isArray(obj) && obj.length===0){
+    result += "[]";
+  } else if(Array.isArray(obj)){
+    result += '[';
+    for (var i=0; i<obj.length; i++){
+      result += stringifyJSON(obj[i]);  
+      if (i!== obj.length-1){
+          result += ',';
+        }
+    }
+    result += ']';
+  } else if (typeof(obj) === 'object') {
+    result += '{';
+    var val = 0;
+    for (var key in obj){
+      val+=1;
+      if(typeof(obj[key])!== 'function' && typeof(obj[key]) !== 'undefined') {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        if (val !== Object.keys(obj).length) {
+          result += ',';
+        }
+      }
+    }
+    result += '}';
+  }
+  return result;
 };
